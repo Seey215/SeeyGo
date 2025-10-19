cd1e16a7074eea8349e9a1bd5f24f81167183d9d
Seey215 <seey215@qq.com>
2025-10-20 01:52:26 +0800

    chore: update Next.js configuration and deployment workflow
    - Change output directory to 'out' and enable trailing slashes in next.config.ts
    - Add basePath and assetPrefix for production environment in next.config.ts
    - Introduce export script in package.json for building and exporting the application
    - Update deployment workflow to use the new output directory and optimize pnpm caching

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  distDir: '.next',
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out',
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/seeygo' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/seeygo/' : '',
 };
 
 export default nextConfig;
